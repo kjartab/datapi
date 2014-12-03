41ecb205c6f3419526a86abd2b0ecc6c380a662d
Kjartan <kjartanbjorset@gmail.com>
2014-12-03 22:52:45 +0100

    gitignore and db template

--- rasters/includes/db_template.php
+++ rasters/includes/db_template.php
@@ -1,12 +1,12 @@
 <?php
 
 $db = array(
-        'connectionString' => 'host=localhost port=5432 dbname=postgres user=postgres password=norkart',
+        'connectionString' => 'host=localhost port=5432 dbname=postgres user=postgres password=kjartan1',
               
         );
 
-$TABLE = 'vestlandet32';
-$SCHEMA = 'public';
+$TABLE = 'norge33';
+$SCHEMA = 'dtm';
 $REQUEST_SRID = '4236';
 $RASTCOL = 'rast';
 
