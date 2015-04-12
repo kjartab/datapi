2b7a8194b7a8289e69b4af314906464affd842f2
Kjartan <kjartanbjorset@gmail.com>
2015-04-12 18:48:12 +0200

    last

--- rasters/includes/DatabaseHelper.php
+++ rasters/includes/DatabaseHelper.php
@@ -164,7 +164,7 @@ Class DatabaseHelper {
 				} else {
 				
 					$query = 'WITH raster as(SELECT ST_Clip(rast,ST_Envelope(ST_Transform(ST_GeomFromText(\''.$outline.'\',' .$requestSRID. '),' .$tableSrid. ')),true) ra from ' .$schema. '.' .$table.' WHERE ST_Intersects(rast,ST_Transform(ST_Envelope(ST_GeomFromText(\''.$outline.'\',' .$requestSRID. ')),' .$tableSrid. ')))		
-					SELECT ST_AsGDALRaster(ST_ReSample(ra,250,250,0,0,0,0,\'algorithm='.$samplingAlgorithm.'\',0.125),\''.$format.'\') from raster';
+					SELECT ST_AsGDALRaster(ra,\''.$format.'\') from raster';
 				
 				}
 				$dbresult = pg_query($query);
